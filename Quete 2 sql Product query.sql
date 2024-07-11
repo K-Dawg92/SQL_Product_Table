48c0d4642fc9baeb7b758800aaa82cc25f70fbb6
Kevin Osei Yaw <kevin.osei.yaw@gmail.com>
2024-07-11 16:41:11 +0200

    Add files via upload

---
+++ Quete 2 sql Product query.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE Product 
+-- (
+--     id INT PRIMARY KEY IDENTITY (1,1),
+--     product_name VARCHAR (50) NOT NULL,
+--     price DECIMAL(10, 2) NULL,
+-- )
+-- CREATE TABLE Grocery
+-- (
+--     id int PRIMARY KEY IDENTITY (1,1),
+--     Total_Price DECIMAL(10, 2) NULL,
+--     Date DATETIME,
+-- )
+-- INSERT INTO Product (product_name,price)
+-- VALUES ('Chicken Breasts',9.00), ('Pasta', 1.10), ('Tomatoes Sauce', 0.90)
+
+-- INSERT INTO Grocery (Total_Price,date)
+-- VALUES (11.00, '2024-07-10 10:22:22 PM'), (0.00, '2024-07-10 10:22:22 PM')
+-- DELETE FROM Product where id in (4,5,6);
+SELECT*FROM Product;
+SELECT*FROM Grocery;
+
